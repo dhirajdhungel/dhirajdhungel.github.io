f5583e08fe8070ca9f548c7eb46d7c2ca039a762
dhirajdhungel <dhirajdhungel@github.com>
2021-06-19 22:16:52 +0545

    Jugaad on grasping sec, SM previews test

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-// const navSlide = function(){
-//     var burger = document.querySelector(".burger"),
-//         nav = document.querySelector('.page-nav'),
-//         navLinks = document.querySelectorAll('nav.page-nav a'); /* target the links inside the nav instead of the nav itself (fixes closing animation) */
+const navSlide = function(){
+  var burger = document.querySelector(".burger"),
+      nav = document.querySelector('.nav-bar');
 
-//         burger.addEventListener("click", function(){
-//         nav.classList.toggle('nav-active');
-
-//         navLinks.forEach((link,index) => {
-//             if (link.style.animation){
-//                 link.style.animation = '';
-//             } else {
-//                 link.style.animation = `navLinkFade 0.2s ease forwards ${index / 3 + 0}s`; /* index / 3 as there are 3 links inside the nav */
-//             }
-//         });
-        
-//         burger.classList.toggle('toggle'); 
-//     });
-// }
-// navSlide();
-
-function myFunction(x) {
-    x.classList.toggle("toggle");
-  }
+      burger.addEventListener("click", function(){
+      nav.classList.toggle('active');
+      burger.classList.toggle('toggle'); 
+  });
+}
+navSlide();
